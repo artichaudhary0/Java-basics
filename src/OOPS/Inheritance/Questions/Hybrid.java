dd1857a4373884b8c56d012f8802d5540c51a1a2
Hardik1282 <chaudhary.aarti1998@gmail.com>
2024-09-25 12:06:23 +0530

    inheritance example

--- src/OOPS/Inheritance/Questions/Hybrid.java
+++ src/OOPS/Inheritance/Questions/Hybrid.java
@@ -1,61 +1,61 @@
-package OOPS.Inheritance.Questions;
-
-/*
-class A
-interface B
-class C  = > A , B
-class D => C(A,B) B
-
- */
-
-class Animals{
-    protected String speciesName;
-
-    public Animals(String speciesName) {
-        this.speciesName = speciesName;
-    }
-
-    public void eat(){
-        System.out.println("eat");
-    }
-}
-
-interface FlyingAnimals{
-    void fly();
-}
-
-
-class  Mammals extends Animals implements FlyingAnimals{
-    public Mammals(String speciesName) {
-        super(speciesName);
-    }
-
-    @Override
-    public void fly() {
-        System.out.println("fly");
-    }
-
-
-    public  void giveBirth()
-    {
-        System.out.println(speciesName + " give birth");
-    }
-}
-
-class  Dogs extends Mammals implements FlyingAnimals{
-    public Dogs(String speciesName) {
-        super(speciesName);
-    }
-
-    @Override
-    public void fly() {
-        System.out.println("fly");
-    }
-
-}
-
-
-
-
-public class Hybrid {
-}
+//package OOPS.Inheritance.Questions;
+//
+///*
+//class A
+//interface B
+//class C  = > A , B
+//class D => C(A,B) B
+//
+// */
+//
+//class Animals{
+//    protected String speciesName;
+//
+//    public Animals(String speciesName) {
+//        this.speciesName = speciesName;
+//    }
+//
+//    public void eat(){
+//        System.out.println("eat");
+//    }
+//}
+//
+//interface FlyingAnimals{
+//    void fly();
+//}
+//
+//
+//class  Mammals extends Animals implements FlyingAnimals{
+//    public Mammals(String speciesName) {
+//        super(speciesName);
+//    }
+//
+//    @Override
+//    public void fly() {
+//        System.out.println("fly");
+//    }
+//
+//
+//    public  void giveBirth()
+//    {
+//        System.out.println(speciesName + " give birth");
+//    }
+//}
+//
+//class  Dogs extends Mammals implements FlyingAnimals{
+//    public Dogs(String speciesName) {
+//        super(speciesName);
+//    }
+//
+//    @Override
+//    public void fly() {
+//        System.out.println("fly");
+//    }
+//
+//}
+//
+//
+//
+//
+//public class Hybrid {
+//}
